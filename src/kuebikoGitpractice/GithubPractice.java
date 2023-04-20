c7fedb23d56ae96f674a589e93c0fd969115828b
hemanta12 <hemantathapa@Hemantas-MacBook-Pro.local>
2023-04-20 08:40:36 -0700

    First upload

---
+++ src/kuebikoGitpractice/GithubPractice.java
@@ -0,0 +1,10 @@
+package kuebikoGitpractice;
+
+public class GithubPractice {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hemantathapa
+ *
+ */
+module kuebikoGitpractice {
+}
